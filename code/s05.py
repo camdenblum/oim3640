9f459639a8206850beb7e003dd7884d6757e86be
camdenblum <cblum2@babson.edu>
2026-02-03 10:53:06 -0500

    Add some inclass work and learning logs

---
+++ code/s05.py
@@ -0,0 +1,17 @@
+# a product would cost $100, how much tax do we pay
+
+product = 100
+tax_rate = 0.0625
+tax = product * tax_rate
+
+print(f'the tax for a product which costs ${product} is ${tax}.')
+
+def calc_tax():
+    product = 100
+    tax_rate = 0.0625
+    tax = product * tax_rate
+    print(f'the tax for a product which costs ${product} is ${tax}.')
+
+
+
+
