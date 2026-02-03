50621e005f211704b9c94d9381d71de492b84c6f
camdenblum <cblum2@babson.edu>
2026-02-03 12:31:46 -0500

    Update s05_review.py

---
+++ code/s05_review.py
@@ -0,0 +1,16 @@
+a = input("Enter a number: ") #how to make sure user inputs an integer
+a = int(a)
+
+a = 5
+
+#check odd or even
+if a % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+a = 2
+1 + a # an expression that evealuates to 3, but it wont actually do anything
+
+print(a + 1) # this will print 3  
+
