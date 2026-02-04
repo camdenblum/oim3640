08f389cc03db86465202f95065288fcd1e3a2764
camdenblum <cblum2@babson.edu>
2026-02-04 18:56:33 -0500

    filling in ch3

--- code/recursive.py
+++ code/recursive.py
@@ -1,6 +1,8 @@
-def groundhog_day():
-    import time
+import time
 
+def groundhog_day():
+
+    """A function that prints 'groundhog day' every section indefiietly."""
     print('Did you mean groundhog day?')
 
     time.sleep(1)
