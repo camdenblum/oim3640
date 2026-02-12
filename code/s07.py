92d4818d0631492641dd54dc1e740d436a8ab232
camdenblum <cblum2@babson.edu>
2026-02-12 12:54:10 -0500

    add s08 in class note and code; some ch 4 solutions

--- code/s07.py
+++ code/s07.py
@@ -5,24 +5,28 @@ def draw_square(turtle_obj, size =100):
     """Draw a square with a given size"""
     for _ in range(4):
             turtle_obj.forward(size)
-            turtle.obj.left(90)
-    
-def main():
-        t=turtle.Turtle
-        t.speed(5)
-        draw_square(5)
-        draw_square(t)
-        draw_square(t,size=50)
-        turtle.mainloop()
+            turtle_obj.left(90)
 
 def draw_spiral(t):
       """draw one square turn a angle then draw another square and repeat"""
       for i in range(36):
         draw_square(t, 50)
         t.left(10)
+    
+def main():
+        t = turtle.Turtle()
+        turtle.speed(0)
+        #draw_square(5)
+        #draw_square(t)
+       # draw_square(t,size=50)
+        draw_spiral(t)
+        turtle.mainloop()
 
 if __name__ == "__main__":
-    main()
+        main()
+
+
+
 
 
 
---
+++ code/s08.py
@@ -0,0 +1,42 @@
+
+result = 0.1 + 0.2
+
+print(result)
+
+print(f'{result:.2f}')
+
+print(f'{result:.3f}')
+      
+
+import math
+
+print(math.floor(3.5))
+
+print(math.floor(-3.99999))
+
+print(math.ceil(3.5))
+
+print(math.ceil(-3.99999))
+
+#boolean examples 
+
+#age = int(input("what is your age? "))
+#if age < 18:
+  #  print('teanager')
+#elif age > 65:
+#    print('senior')
+#else:
+ #   print('adult')
+
+score = 91
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+else:
+    print('F')
+
+
